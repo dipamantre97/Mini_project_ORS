bf46d2a8e20f618fee06a4d13f7f92bcf7ef7c63
dipamantre <dipamantre97@gmail.com>
2022-06-08 16:03:53 +0530

    header for voting.c

---
+++ voting.h
@@ -0,0 +1,21 @@
+
+//*******************************************Module Description*******************************************************
+
+/*
+1. Module name: Voting module
+2. Date: 27-05-2022
+3. Function name: voting()
+3. Author name: Dipa mantre
+4. Description: The purpose of this function is to display the voting menu and call voting request and voting response function.
+5. Different functions supported in this module : voting()
+5. Global variables accessed by module: No global variables accessed
+*/
+
+//**************************************Header files****************************************
+
+#include"main.h"
+
+//***********************************Function declarations****************************************
+
+void voting();
+
