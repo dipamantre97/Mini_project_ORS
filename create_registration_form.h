3ab34372a809b6bf998f4b45e1e22c9d26b49d9e
dipamantre <dipamantre97@gmail.com>
2022-06-08 15:56:49 +0530

    header for create_registration_form.c file

---
+++ create_registration_form.h
@@ -0,0 +1,15 @@
+//*******************************************Module Description*******************************************************
+
+/*
+1. Module name: Training registration module
+2. Date: 27-05-2022
+3. Function name: create_training_registration_form
+3. Author name: Chiluka Harish(
+4. Description: The purpose of this module is to create training registration form when user wants to create excel sheet by taking the user inputs for fields.
+5. Different functions supported in this module: create_training_registration_form()
+6. Global variables accessed by module: Forms_count and Forms
+7. Modification/History: https://github.com/Harishchiluka0111/Mini_ORS_1_june
+*/
+//***********************************Function declarations****************************************
+
+void create_training_registration_form(Employee_training_data **first_field);
