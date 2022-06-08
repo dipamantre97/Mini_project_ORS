5f17ae717c5ff9b3c095f715f4da4aaecf787472
dipamantre <dipamantre97@gmail.com>
2022-06-08 16:06:46 +0530

    header for create_voting_req_form.c file

---
+++ create_voting_req_form.h
@@ -0,0 +1,20 @@
+
+//*******************************************Module Description*******************************************************
+
+/*
+1. Module name: Voting request module
+2. Date: 27-05-2022
+3. Function name: create_voting_req_form
+3. Author name: Dipa mantre
+4. Description: The purpose of this module is to create voting request form for the user and writes the fields names created by the user to excel sheet. 
+5. Different functions supported in this module: create_voting_req_form()
+6. Global variables accessed by module: Count_forms and Forms
+*/
+
+//**************************************Header files****************************************
+
+#include"main.h"
+
+//***********************************Function declarations****************************************
+
+void create_voting_req_form(Vote **root);
