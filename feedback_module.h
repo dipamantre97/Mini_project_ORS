9095f45e4fae48e6047443cc87a891a3fce13834
dipamantre <dipamantre97@gmail.com>
2022-06-08 16:01:32 +0530

    header for feedback_module.c file

---
+++ feedback_module.h
@@ -0,0 +1,20 @@
+/*****************************************DESCRIPTION*******************************************************************/
+
+/*
+
+1. Name of the module                           : feedback_module
+2. Date of creation                             : 27/5/2022
+3. Author of Module                             : Tejaswini S P
+4. Description of module                        : The purpose of this module is to display the conducted trainings and after the user selects the particular training it will call the feedback function to record the user given feedback
+ 
+5. Different functions supported in this module : feedback_module()
+6.Github link                                   : https://github.com/Harishchiluka0111/Mini_ORS_1_june
+*/
+
+/**********************************************************************************************************************/
+
+/**************************HEADER FILES*****************************/
+#include"main.h"
+
+/*************************FUNCTION PROTOTYPES**********************/
+void feedback_module();  //display the available conducted training forms and calls the feedback function to give feedback for the selected training form          
