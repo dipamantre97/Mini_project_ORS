5de464c1e6ff3b8d536b1ac568054236f82fb39a
dipamantre <dipamantre97@gmail.com>
2022-06-08 15:59:00 +0530

    header for delete_registration_form.c file

---
+++ delete_registration_form.h
@@ -0,0 +1,17 @@
+//*******************************************Module Description*******************************************************
+
+/*
+1. Module name: Training registration module
+2. Date: 27-05-2022
+3. Function name: delete_training_registration_form
+3. Author name: Chiluka Harish
+4. Description: The purpose of this module is to delete the  training registration form which is created based on the option selected by the user.
+5. Different functions supported in this module: delete_training_registration_form()
+6. Global variables accessed by module: Forms_count and Forms
+7. Modification/History: https://github.com/Harishchiluka0111/Mini_ORS_1_june
+*/
+
+
+//***********************************Function declarations****************************************
+
+void delete_training_registration_form(Employee_training_data **first_field);
