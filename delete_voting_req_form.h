70f5202bb6bfc38433774ba6d1df0953937013ad
dipamantre <dipamantre97@gmail.com>
2022-06-08 16:08:56 +0530

    header for delete_voting_req_form.c file

---
+++ delete_voting_req_form.h
@@ -0,0 +1,20 @@
+
+//*******************************************Module Description*******************************************************
+
+/*
+1. Module name: Voting request module
+2. Date: 27-05-2022
+3. Function name: delete_voting_req_form
+3. Author name: Dipa mantre
+4. Description: The purpose of this module is to delete voting request form created by the user.
+5. Different functions supported in this module: delete_voting_req_form()
+6. Global variables accessed by module: Count_forms and Forms
+*/
+
+//**************************************Header files****************************************
+
+#include"main.h"
+
+//***********************************Function declarations****************************************
+
+void delete_voting_req_form(Vote **root);
